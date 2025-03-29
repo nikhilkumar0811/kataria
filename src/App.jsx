022677870a978b40344b3cf4756614cc101a9da6
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2025-03-29 22:14:00 +0530

    update 35

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import Navbar from "./components/Navbar/Navbar.jsx";
-// import Hero from "./components/Hero/Hero.jsx";
-// import Testimonials from "./components/Testimonials/Testimonials.jsx";
-// import Footer from "./components/Footer/Footer.jsx";
-// import "./index.css";
-
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Hero />
-//       <Testimonials />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
